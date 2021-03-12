797e70a9ce996989dd595902c6de9352c25f0f72
Troy Clarke <HX13978@wdxhx13978vcg.home.network>
2021-03-12 15:26:27 -0600

    Axios call to backend successfull

---
+++ src/Components/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Link, Switch } from "react-router-dom";
+import axios from "axios";
+
+const backendUrl =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:3000/activities";
+// const backendUrl = FOR HEROKU WHEN DONE
+
+
+export default class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      activities: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`${backendUrl}`).then((response) => {
+      this.setState({
+        activities: response.data.activities,
+      });
+    });
+  }
+
+  
+
+  render() {
+        console.log(this.state.activities);
+    return (
+      <div className="App">
+        <h1>All Activities</h1>
+  
+      </div>
+    );
+  }
+}
